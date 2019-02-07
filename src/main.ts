6018b97d61ff79132c773d88caa840a9cab1111f
antoineraoulimpargo <a.raoul@impargo.de>
2019-02-07 14:59:53 +0200

    add enum for supported languages

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import Circle from "./Circle";
 import HEREMap from "./HEREMap";
 import Marker from "./Marker";
 import Route from "./Route";
+import { Language } from "./utils/languages";
 export {
   Circle,
   HEREMap,
   Marker,
   Route,
+  Language,
 };
 
 export default HEREMap;
